45213308129484169ef8df2b7d66ee5f534aac9f
Derek Maggio <pearappllc@gmail.com>
2024-03-26 14:13:38 -0400

    Creation of new template favorite-course.html with its appropriate route in app.py file that accepted two parameters via url that printed out the "You entered your favorite course as:" message

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,15 @@ def about():
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('Your favorite course name is: ' + request.args.get('course_name'))
+    print('Your favorite course number is: ' + request.args.get('course_number'))
+    print('You entered your favorite course as: ' + request.args.get('course_name') +
+          request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
 
 if __name__ == '__main__':
     app.run()
