2175ab04ec92cbb01c961a99765001a49f33cfcc
Derek Maggio <pearappllc@gmail.com>
2024-04-02 17:24:16 -0400

    Added in functional database connectivity and edited student_entry and student_view_all html code to have email logic integrated in it.

--- create_db.py
+++ create_db.py
@@ -1,62 +1,34 @@
 from app import app, db
-from models import Product, ProductCategory, Messages, Size
+from models import Student, Major
+import datetime as dt
 
 with app.app_context():
     db.drop_all()
     db.create_all()
 
-    # Initial loading of product categories
-    product_categories = [
-        {'category_id': 1, 'category_name': 'Hats', 'size_metric': 'Circumference(in)'},
-        {'category_id': 2, 'category_name': 'Shirts', 'size_metric': 'Chest'},
-        {'category_id': 3, 'category_name': 'Pants', 'size_metric': 'Waist'},
-    ]
-
-    for each_product_category in product_categories:
-        print(f'{each_product_category["category_name"]} inserted into product_category')
-        a_product_category = ProductCategory(category_id=each_product_category['category_id'],
-                                             category_name=each_product_category['category_name'],
-                                             size_metric=each_product_category['size_metric'])
-        db.session.add(a_product_category)
+    # Initial loading of majors
+    majors = ['Accounting', 'Finance', 'Information Systems', 'International Business', 'Management', \
+              'Operations Management & Business Analytics', 'Supply Chain Management']
+    for each_major in majors:
+        print(each_major)
+        a_major = Major(major=each_major)
+        db.session.add(a_major)
         db.session.commit()
 
-    # Initial loading of products
-    products = [
-        {'product_name':'Red Hat', 'product_code':'PROD-0555', 'product_description':'Show your Terp spirit by wearing this stylish Red hat.',
-            'product_image':'PROD-0555-hat_red.jpg', 'product_price':12.95, 'category_id': 1},
-        {'product_name': 'Black Hat', 'product_code': 'PROD-0777',
-         'product_description': 'Don\'t let a bad hair day hold you back from cheering on the Terps! This hat is your cap-tain solution for stylish team spirit.',
-         'product_image': 'PROD-0777-hat_black.jpg', 'product_price': 14.99, 'category_id': 1},
-        {'product_name': 'Men\'s Terrapin Shirt', 'product_code': 'PROD-0123', 'product_description': 'This Men\'s Maryland Terrapin shirt is your shell-ter for spontaneous beach weekend getaways!',
-            'product_image': 'PROD-0123-shirt.jpg', 'product_price':39.99, 'category_id': 2},
-        {'product_name': 'Boracay Shorts', 'product_code': 'PROD-0987', 'product_description': 'Skip the chips and dips, and rock your beach trips! These shorts are perfect for cheering on the team!',
-            'product_image': 'PROD-0987-shorts.png', 'product_price':25.49, 'category_id': 3},
+    # Initial loading of students first_name, last_name, major_id, birth_date, email, is_honors
+    students = [
+        {'student_id': '1', 'first_name': 'Robert', 'last_name':'Smith', 'major_id':3,
+            'birth_date': dt.datetime(2007, 6, 1), 'email':'roberthsmith@umd.edu', 'is_honors':1},
+        {'student_id': '2', 'first_name': 'Leo', 'last_name': 'Van Munching', 'major_id':6,
+         'birth_date': dt.datetime(2008, 3, 24), 'email':'leovanmunching@umd.edu', 'is_honors': 0},
     ]
 
-    for each_product in products:
-        print(f'{each_product["product_name"]} inserted into product')
-        a_product = Product(product_name=each_product['product_name'], product_code=each_product['product_code'],
-                            product_description=each_product['product_description'], product_image=each_product['product_image'],
-                            product_price=each_product['product_price'], category_id=each_product['category_id'])
-        db.session.add(a_product)
+    for each_student in students:
+        print(f'{each_student["first_name"]} {each_student["last_name"]} inserted into Student')
+        a_student = Student(first_name=each_student["first_name"], last_name=each_student["last_name"],
+                            major_id=each_student["major_id"], birth_date=each_student["birth_date"],
+                            email=each_student["email"], is_honors=each_student["is_honors"])
+        db.session.add(a_student)
         db.session.commit()
 
-    category_sizes = [
-        {'category_id': 1, 'size': 'S/M', 'value': '20.8 - 21.5'},
-        {'category_id': 1, 'size': 'M/L', 'value': '22 - 23'},
-        {'category_id': 1, 'size': 'L/XL', 'value': '23 - 24.2'},
-        {'category_id': 2, 'size': 'S', 'value': '34.5 - 36'},
-        {'category_id': 2, 'size': 'M', 'value': '36.5 - 40'},
-        {'category_id': 2, 'size': 'L', 'value': '40.5 - 44'},
-        {'category_id': 2, 'size': 'XL', 'value': '44.5 - 48'},
-        {'category_id': 3, 'size': 'S', 'value': '28-30'},
-        {'category_id': 3, 'size': 'M', 'value': '31-33'},
-        {'category_id': 3, 'size': 'L', 'value': '34-36'},
-    ]
 
-    for each_category_size in category_sizes:
-        print(f'{each_category_size["size"]} for Category {each_category_size["category_id"]} inserted into size')
-        a_size = Size(size=each_category_size['size'], value=each_category_size['value']
-                            , category_id=each_category_size['category_id'])
-        db.session.add(a_size)
-        db.session.commit()
